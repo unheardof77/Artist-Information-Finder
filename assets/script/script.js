3a2db6fb7d13005027806192b826228ef0d5fa03
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-28 11:40:19 -0700

    Fetched top 10 artist tracks.  Created function to display tracks.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,26 @@
-let searchedArtist = document.querySelector(`input`).value;
+let searchedArtist = document.querySelector(`input`).value.trim().replaceAll(` `, `-`);
 
 function top10ArtistTracks(){
-
+    fetch(`https://theaudiodb.com/api/v1/json/523532/track-top10.php?s=${searchedArtist}`)
+        .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+                    displayTop10Tracks(data, searchedArtist);
+                })
+            }else{
+                //We'll have it run a error page.
+            };
+        })
+        .catch(function(error){
+            //have it tell them we cannot find the artist they searched for.
+        })
 };
 
-function top10ArtistAlbum(){
-    
-};
+function displayTop10Tracks(data){
+    let $ul = document.getElementById(`idOfUl`);
+    for(i=0; i < 10; i++ ){
+        let $li = document.createElement(`li`);
+        $li.textContent = data.track[i].strTrack;
+        $ul.appendChild($li);
+    }
+}
