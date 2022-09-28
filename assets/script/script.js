8eeb97ea272dd5a84393f2b52b93a1c7e21c0668
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-28 14:18:39 -0700

    Added event listener for submit.  Made artist name appear.

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,24 +8,27 @@ let formSubmit = document.querySelector(`form`);
 function searchAllApi(event){
     event.preventDefault();
     let searchedArtist = document.querySelector(`input`).value.trim().replaceAll(` `, `+`);
+    $artistName = document.getElementById(`artistName`)
+    $artistName.textContent = ""
+    $artistName.textContent = searchedArtist.replaceAll(`+`, ` `);
     top10ArtistTracks(searchedArtist);
     relatedArtistData(searchedArtist);
 };
 //fetches information for top 10 artist
 function top10ArtistTracks(searchedArtist){
     fetch(`https://theaudiodb.com/api/v1/json/523532/track-top10.php?s=${searchedArtist}`)
-        .then(function(response){
-            if(response.ok){
-                response.json().then(function(data){
-                    displayTop10Tracks(data, searchedArtist);
-                })
-            }else{
-                //We'll have it run a error page.
-            };
-        })
-        .catch(function(error){
-            //have it tell them we cannot find the artist they searched for.
-        })
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayTop10Tracks(data, searchedArtist);
+            })
+        }else{
+            //We'll have it run a error page.
+        };
+    })
+    .catch(function(error){
+        //have it tell them we cannot find the artist they searched for.
+    })
 };
 
 
@@ -40,9 +43,9 @@ function displayTop10Tracks(data){
 }
 
 const options = {
-	method: 'GET',
+    method: 'GET',
 	headers: {
-		'X-RapidAPI-Key':'08d6ec9a26mshf44c883a36529a8p13ff3ejsn3ee88f8bb6b3',
+        'X-RapidAPI-Key':'08d6ec9a26mshf44c883a36529a8p13ff3ejsn3ee88f8bb6b3',
 		'X-RapidAPI-Host': 'genius.p.rapidapi.com'
 	}
 };
@@ -50,8 +53,9 @@ const options = {
 
 function relatedArtistData(searchedArtist) {
     fetch(`https://genius.p.rapidapi.com/search?q=${searchedArtist}${options}`)
-	    .then(response => response.json())
-	    .then(response => console.log(response))
-	    .catch(err => console.error(err))
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
 };
 
+formSubmit.addEventListener(`submit`, searchAllApi);
