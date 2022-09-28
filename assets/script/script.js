69aa03a72d9de601563f430cad82f1cde1ce30d3
IAMFALZIII <flop3z.11@gmail.com>
2022-09-28 13:11:34 -0700

    Saving before pull

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,12 @@
-let formSubmit = document.querySelector(`form`);
-//Controls all search functions.
-function searchAllApi(event){
-    event.preventDefault();
-    let searchedArtist = document.querySelector(`input`).value.trim().replaceAll(` `, `-`);
-    top10ArtistTracks(searchedArtist);
-};
-//fetches information for top 10 artist
+let searchedArtist = document.querySelector(`search`).value;
+replaceAll(` `, `-`);
+
 function top10ArtistTracks(){
     fetch(`https://theaudiodb.com/api/v1/json/523532/track-top10.php?s=${searchedArtist}`)
         .then(function(response){
             if(response.ok){
                 response.json().then(function(data){
-                    displayTop10Tracks(data);
-                    return
+                    displayTop10Tracks(data, searchedArtist);
                 })
             }else{
                 //We'll have it run a error page.
@@ -22,18 +16,19 @@ function top10ArtistTracks(){
             //have it tell them we cannot find the artist they searched for.
         })
 };
-//Displays the information gathered from top10ArtistTracks.
+
+function top10ArtistAlbum(){
+    
+};
+
 function displayTop10Tracks(data){
-    let $ul = document.getElementById(`bestOf`);
+    let $ul = document.getElementById(`bestof`);
     for(i=0; i < 10; i++ ){
         let $li = document.createElement(`li`);
         $li.textContent = data.track[i].strTrack;
         $ul.appendChild($li);
     }
-};
-
-formSubmit.addEventListener(`submit`, searchAllApi);
-
+}
 
 const options = {
 	method: 'GET',
@@ -43,11 +38,7 @@ const options = {
 	}
 };
 
-function relatedArtistData(searchedArtist){
-    fetch(`https://genius.p.rapidapi.com/search?q=${searchedArtist}${options}`)
-	    .then(response => response.json())
-	    .then(response => console.log(response))
-	    .catch(err => console.error(err));
-
-        
-}
+fetch('https://genius.p.rapidapi.com/search?q=' + (searchedArtist), options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
